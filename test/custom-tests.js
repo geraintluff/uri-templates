6421985ef22c70515b4264247356e1a59bd95b82
Geraint Luff <luffgd@gmail.com>
2015-09-30 18:00:41 +0100

    Version bump

--- test/custom-tests.js
+++ test/custom-tests.js
@@ -5,7 +5,7 @@ describe("Guessing variable priority", function () {
 	it('GitHub issue #8', function () {
 		var template = new UriTemplate("{+path}/c/capture{/date,id,page}");
 		var guess = template.fromUri('/a/b/c/capture/20140101/1');
-		
+
 		// we already test elsewhere that this reconstructs correctly - we just want to make sure variables are prioritised left-to-right
 		assert.strictEqual(guess.date, '20140101');
 		assert.strictEqual(guess.id, '1');
@@ -29,7 +29,7 @@ describe("Query optional when decoding", function () {
 		var uri = '/user/1,2,3/posts';
 		var guess = template.fromUri(uri);
 		assert.isObject(guess);
-		
+
 		var trimmed = template.fill(guess).replace(/\?$/, '');
 		assert.strictEqual(trimmed, uri);
 	});
